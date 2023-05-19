9bb3fcd3976be188c5a5ec9b0cc67786d0beccad
slashsbluesball <47663169+slashsbluesball@users.noreply.github.com>
2023-05-19 19:31:43 +0300

    readme and swagger added (#2)
    Co-authored-by: svasuada <svasuada@centos9-vmw.eu.tieto.com>

--- src/main/java/com/happeo/userprovisioning/demo2api/Demo2ApiApplication.java
+++ src/main/java/com/happeo/userprovisioning/demo2api/Demo2ApiApplication.java
@@ -3,9 +3,10 @@ package com.happeo.userprovisioning.demo2api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 
-
+// @EnableWebMvc
 // @SpringBootApplication
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class Demo2ApiApplication {
